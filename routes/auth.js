6de5d674215d70ea642aec4c73f12ee8e0ce3a6f
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-30 10:03:21 -0700

    added auth route

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+'use strict';
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
+const router = express.Router();
+
+const localAuth = passport.authenticate('local', { session: false, failWithError: true });
+
+// The user provides a username and password to login
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
+});
+
+const jwtAuth = passport.authenticate('jwt', { session: false, failWithError: true });
+
+// The user exchanges a valid JWT for a new one with a later expiration
+router.post('/refresh', jwtAuth, (req, res) => {
+  const authToken = createAuthToken(req.user);
+  res.json({authToken});
+});
+
+function createAuthToken(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
+module.exports = router;
