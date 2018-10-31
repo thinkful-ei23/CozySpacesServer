ba66d21085f46517ec1b90eb253c17931f316c8e
Burkey Koontz <bk.koontz@gmail.com>
2018-10-31 13:19:45 -0700

    added routes

--- routes/places.js
+++ routes/places.js
@@ -1,22 +1,44 @@
 'use strict';
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-const Place = require('../models/places');
+// const Place = require('../models/places');
 
 const router = express.Router();
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
+  const seedData = [{id: 123, name: 'yolanda\'s', type: 'coffee shop', overallRating: 6}];
 
-  Request.find()
-    // .sort({ })
-    .then(results => {
-      res.json(results);
-    })
-    .catch(err => {
-      next(err);
-    });
+  res.json(seedData);
+  // Request.find()
+  //   // .sort({ })
+  //   .then(results => {
+  //     res.json(results);
+  //   })
+  //   .catch(err => {
+  //     next(err);
+  //   });
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  // if (!mongoose.Types.ObjectId.isValid(id)) {
+  //   const err = new Error('The `id` is not valid');
+  //   err.status = 400;
+  //   return next(err);
+  // }
+  res.json({id: 123, name: 'yolanda\'s', type: 'coffee shop', overallRating: 6});
+  // Place
+  //   .findOne({_id: id})
+  //   .then(result => {
+  //     res.json(result);
+  //   })
+  //   .catch(err => {
+  //     next(err);
+  //   });
+ 
 });
 
 module.exports = router;
