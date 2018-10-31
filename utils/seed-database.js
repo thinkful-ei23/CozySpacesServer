85cb7bfcfbbb46674a0e47f104138930d1de45ce
Selvin Norman <selvin715@gmail.com>
2018-10-31 12:50:01 -0700

    added user seed data, drop database and seed database utils

---
+++ utils/seed-database.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const { DATABASE_URL } = require('../config');
+
+const Places = require('../models/places');
+const User = require('../models/users');
+const Rating = require('../models/ratings');
+const Photo = require('../models/photos');
+
+const seedPlaces = require('../db/seed/places');
+const seedUsers = require('../db/seed/users');
+const seedRatings = require('../db/seed/ratings');
+const seedPhotos = require('../db/seed/photos');
+
+console.log(`Connecting to mongodb at ${DATABASE_URL}`);
+mongoose.connect(DATABASE_URL)
+  .then(() => {
+    console.info('Dropping Database');
+    return mongoose.connection.db.dropDatabase();
+  })
+  .then(() => {
+    console.info('Seeding Database');
+    return Promise.all([
+
+      Subscription.insertMany(seedSubscriptions),
+      Subscription.createIndexes(),
+
+      User.insertMany(seedUsers),
+      User.createIndexes()
+
+    ]);
+  })
+  .then(() => {
+    console.info('Disconnecting');
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(err);
+    return mongoose.disconnect();
+  });
