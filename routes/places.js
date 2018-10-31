84a2b617636e2b5ad18c87d69d7d92f19441a5b0
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-31 09:52:56 -0700

    server.js and places router hooked up

---
+++ routes/places.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Place = require('../models/places');
+
+const router = express.Router();
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+
+  Request.find()
+    // .sort({ })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
