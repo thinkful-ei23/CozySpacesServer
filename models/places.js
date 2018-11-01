980443f3369b9e7f3c4439e1b00c79c3dd265c97
Burkey Koontz <bk.koontz@gmail.com>
2018-11-01 08:58:22 -0700

    changed place schema to include breakdown of overall ratings

--- models/places.js
+++ models/places.js
@@ -8,8 +8,14 @@ const placeSchema = new mongoose.Schema({
   address: { type: String, required: true },
   city: { type: String, required: true },
   state: { type: String, required: true },
-  zipcode: { type: String, required: true, default: '', },
-  averageCozyness:  { type: Number },
+  zipcode: { type: String, required: true, default: '' },
+  averageCozyness: {type: Number},
+  averageWarmLighting: {type: Number},
+  averageRelaxedMusic: {type: Number},
+  averageCalmEnvironment: {type: Number},
+  averageSoftFabrics: {type: Number},
+  averageComfySeating: {type: Number},
+  averageHotFoodDrink: {type: Number},
   photos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Photo' }],
   ratings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Rating'}]
 });
