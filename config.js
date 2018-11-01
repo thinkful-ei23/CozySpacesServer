c63cc683494a3c1cef6e7bdbf83395c4d9452d7e
Selvin Norman <selvin715@gmail.com>
2018-10-31 19:32:24 -0700

    testing seed-database

--- config.js
+++ config.js
@@ -1,14 +1,32 @@
 'use strict';
 
+// require('dotenv').config();
+
+// module.exports = {
+//   PORT: process.env.PORT || 8080,
+//   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+//   DATABASE_URL: process.env.DATABASE_URL,
+//   TEST_DATABASE_URL:
+//     process.env.TEST_DATABASE_URL ||
+//     'mongodb://localhost/thinkful-backend-test',
+//   JWT_SECRET: process.env.JWT_SECRET,
+//   JWT_EXPIRY: process.env.JWT_EXPIRY || '1d'
+// };
+
+
+
 require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    'mongodb://dev:thinkful1@ds147723.mlab.com:47723/cozy-spaces',
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/thinkful-backend-test',
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '1d'
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
+
