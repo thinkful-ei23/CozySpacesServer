cc0f3fd492171aebbe19523a5d4abe6206069cac
Alex Puhl <apuhlswimmer@hotmail.com>
2018-11-01 11:12:15 -0600

    changed route to handle jwt token refresh

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/cozy-spaces',
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/thinkful-backend-test',
