609f26dbe96957841084892456475a84b71bde59
Selvin Norman <selvin715@gmail.com>
2018-11-01 13:54:38 -0700

    added user ratings

--- models/users.js
+++ models/users.js
@@ -6,7 +6,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true},
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  ratings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Rating'}]
 });
 
 userSchema.set('toObject', {
