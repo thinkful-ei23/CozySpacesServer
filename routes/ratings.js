bd8490ad9a2d62fd982eb6f74ff39f6aaa0e3332
Selvin Norman <selvin715@gmail.com>
2018-11-06 09:39:47 -0800

    added coding to calculate averages and update

--- routes/ratings.js
+++ routes/ratings.js
@@ -34,7 +34,7 @@ router.get('/', (req, res, next) => {
     filter.userId = userId;
   }
   Rating.find(filter)
-  //Rating.find(filter) //
+    //Rating.find(filter) //
     .sort({ updatedAt: 'desc' })
     .then(results => {
       console.log('results: ', results);
@@ -109,6 +109,7 @@ router.post('/', (req, res, next) => {
         return next(err);
       }
       return Rating.create(newRating).then(result => {
+        updateAvgRatings(placesLink);
         res
           .location(`${req.originalUrl}/${result.id}`)
           .status(201)
@@ -151,11 +152,12 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  Rating.findOne({_id: id})
+  Rating.findOne({ _id: id })
     .then(rating => {
       if (rating) {
         rating.rating = updateRating.rating;
         rating.save();
+        updateAvgRatings(id);
         res.json(rating);
       } else {
         next();
@@ -164,7 +166,7 @@ router.put('/:id', (req, res, next) => {
     .catch(err => {
       next(err);
     });
-  
+
 });
 /* ========== DELETE/REMOVE A SINGLE ITEM ========== */
 router.delete('/:placesLink', (req, res, next) => {
@@ -181,6 +183,7 @@ router.delete('/:placesLink', (req, res, next) => {
   Rating.findOneAndDelete({ placesLink, userLink })
     .then(result => {
       if (result) {
+        updateAvgRatings(placesLink);
         res.sendStatus(204);
       } else {
         res.sendStatus(404);
@@ -190,5 +193,62 @@ router.delete('/:placesLink', (req, res, next) => {
       next(err);
     });
 });
+
+function updateAvgRatings(placeId) {
+  let newwarmLightingScore,
+    newRelaxedMusicScore,
+    newCalmEnvironmentScore,
+    newSoftFabricsScore,
+    newComfySeatingScore,
+    newHotFoodDrinkScore = 0;
+
+  let warmLightingScore,
+    relaxedMusicScore,
+    calmEnvironmentScore,
+    softFabricsScore,
+    comfySeatingScore,
+    hotFoodDrinkScore = 0;
+
+  Ratings.find({ placesLink: placeId })
+    .then((ratings) => {
+
+      let numberOfRatings = ratings.length;
+      ratings.forEach((rating) => {
+        warmLightingScore += rating.rating.warmLighting;
+        relaxedMusicScore += rating.rating.relaxedMusic;
+        calmEnvironmentScore += rating.rating.calmEnvironment;
+        softFabricsScore += rating.rating.softFabrics;
+        comfySeatingScore += rating.rating.comfySeating;
+        hotFoodDrinkScore += rating.rating.hotFoodDrink;
+      });
+      newWarmLightingScore = Math.floor(warmLightingScore / numberOfRatings);
+      newRelaxedMusicScore = Math.floor(relaxedMusicScore / numberOfRatings);
+      newCalmEnvironmentScore = Math.floor(warmLightingScore / numberOfRatings);
+      newSoftFabricsScore = Math.floor(warmLightingScore / numberOfRatings);
+      newComfySeatingScore = Math.floor(warmLightingScore / numberOfRatings);
+      newHotFoodDrinkScore = Math.floor(warmLightingScore / numberOfRatings);
+      return Place.findOne({ _id: placeId })
+    })
+    .then((place) => {
+      place.averageWarmLighting = newWarmLightingScore;
+      place.averageRelaxedMusic = newRelaxedMusicScore;
+      place.averageCalmEnvironment = newCalmEnvironmentScore;
+      place.averageSoftFabrics = newSoftFabricsScore;
+      place.averageComfySeating = newComfySeatingScore;
+      place.averageHotFoodDrink = newHotFoodDrinkScore;
+      place.averageCozyness = 
+      Math.floor(
+        (
+          +place.averageCalmEnvironment +
+          +place.averageSoftFabrics +
+          +place.averageComfySeating +
+          +place.averageHotFoodDrink +
+          +place.averageHotFoodDrink
+        ) / 6);
+      place.save();
+    })
+    .catch((err) => console.error(err));
+}
+
 
 module.exports = router;
